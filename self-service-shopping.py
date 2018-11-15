d825e7d699282e20a79eb788b3e7dbc88812ed7d
Agustinus Baskara <agtbaskara@gmail.com>
2018-11-15 13:29:32 +0700

    Add Working Program

--- self-service-shopping.py
+++ self-service-shopping.py
@@ -2,138 +2,159 @@ import face_recognition
 import cv2
 import pyzbar.pyzbar as pyzbar
 import numpy as np
+import time
+import os
+from threading import Thread
 
+def decode_qr(input_frame):
+    """
+    Decode QR in frame return a list of detected QR
+    """
+    detected_list = []
+    for i in range (0,30):
+        decodedObjects = pyzbar.decode(input_frame)
+        for obj in decodedObjects:
+            if obj.data.decode('ascii') not in detected_list:
+                detected_list.append(obj.data.decode('ascii'))
+    return detected_list
 
-def decode(im) : 
-  # Find barcodes and QR codes
-  decodedObjects = pyzbar.decode(im)
- 
-  # Print results
-  for obj in decodedObjects:
-    print('Type : ', obj.type)
-    print('Data : ', obj.data)
-     
-  return decodedObjects
- 
- 
-# Display barcode and QR code location  
-def display(im, decodedObjects):
- 
-  # Loop over all decoded objects
-  for decodedObject in decodedObjects: 
-    points = decodedObject.polygon
- 
-    # If the points do not form a quad, find convex hull
-    if len(points) > 4 : 
-      hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
-      hull = list(map(tuple, np.squeeze(hull)))
-    else : 
-      hull = points
-     
-    # Number of points in the convex hull
-    n = len(hull)
- 
-    # Draw the convext hull
-    for j in range(0,n):
-      cv2.line(im, hull[j], hull[ (j+1) % n], (255,0,0), 3)
- 
-  # Display results 
-  cv2.imshow("Results", im)
+class FrameCapture:
+    """
+    Class for getting frame from cap
+    """
+    def __init__(self):
+        self.video_capture_face = cv2.VideoCapture(0)
+        self.video_capture_qr = cv2.VideoCapture(1)
+        (self.grabbed_face, self.frame_face) = self.video_capture_face.read()
+        (self.grabbed_qr, self.frame_qr) = self.video_capture_qr.read()
+        self.stopped = False
 
+        self.frame_face_small = cv2.resize(self.frame_face.copy(), (0, 0), fx=0.25, fy=0.25)
+        self.frame_qr_small = cv2.resize(self.frame_qr.copy(), (0, 0), fx=0.25, fy=0.25)
+        self.preview = np.concatenate((self.frame_face_small, self.frame_qr_small), axis=1)
+    
+    def start(self):
+        """
+        Start get thread
+        """
+        Thread(target = self.get, args = ()).start()
+        return self
 
+    def stop(self):
+        """
+        Stop Image Processing thread
+        """
+        self.stopped = True
+        self.video_capture_face.release()
+        self.video_capture_qr.release()
+    
+    def get(self):
+        """
+        Thread to capture frame from camera
+        """
+        while not self.stopped:
+            if not (self.grabbed_face or self.grabbed_qr):
+                self.stop()
+            else:
+                (self.grabbed_face, self.frame_face) = self.video_capture_face.read()
+                (self.grabbed_qr, self.frame_qr) = self.video_capture_qr.read()
+            self.frame_face_small = cv2.resize(self.frame_face.copy(), (0, 0), fx=0.7, fy=0.7)
+            self.frame_qr_small = cv2.resize(self.frame_qr.copy(), (0, 0), fx=0.7, fy=0.7)
+            self.preview = np.concatenate((self.frame_face_small, self.frame_qr_small), axis=0)
+            cv2.imshow('Preview', self.preview)
+            cv2.waitKey(1)
+                
+# Initialize Frame Capture Thread
+FrameThread = FrameCapture()
+FrameThread.start()
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
+# Load Face Image
+a_image = face_recognition.load_image_file("face/a.jpg")
+b_image = face_recognition.load_image_file("face/b.jpg")
+c_image = face_recognition.load_image_file("face/c.jpg")
 
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+# Recognize Face Image
+a_face_encoding = face_recognition.face_encodings(a_image)[0]
+b_face_encoding = face_recognition.face_encodings(b_image)[0]
+c_face_encoding = face_recognition.face_encodings(c_image)[0]
 
-# Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
-video_captureqr = cv2.VideoCapture(1)
-
-# Load a sample picture and learn how to recognize it.
-baskara_image = face_recognition.load_image_file("me.jpg")
-baskara_face_encoding = face_recognition.face_encodings(baskara_image)[0]
-
-# Create arrays of known face encodings and their names
+# Create List Of Face Encoding
 known_face_encodings = [
-    baskara_face_encoding
-]
-known_face_names = [
-    "Baskara"
+    a_face_encoding,
+    b_face_encoding,
+    c_face_encoding
 ]
 
-# Initialize some variables
+# Create List Names
+known_face_names = [
+    "Alpha",
+    "Baskara",
+    "Charlie"
+]
+
+# Create List Saldo
+saldo = [
+    90000,
+    20000,
+    30000
+]
+
+# Initialize variable
 face_locations = []
 face_encodings = []
 face_names = []
-process_this_frame = True
 
 while True:
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
-    retqr, frameqr = video_captureqr.read()
+    # Get Frame from Frame Capture Thread
+    frame_face = FrameThread.frame_face
 
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    # Resize then convert to RGB
+    small_frame = cv2.resize(frame_face, (0, 0), fx=0.25, fy=0.25)
     rgb_small_frame = small_frame[:, :, ::-1]
 
-    # Only process every other frame of video to save time
-    if process_this_frame:
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+    # Detect All Face
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-        face_names = []
-        for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-            name = "Unknown"
+    face_names = []
+    for face_encoding in face_encodings:
+        # Match Face
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "Unknown"
 
-            # If a match was found in known_face_encodings, just use the first one.
-            if True in matches:
-                first_match_index = matches.index(True)
-                name = known_face_names[first_match_index]
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = known_face_names[first_match_index]
 
-            face_names.append(name)
+        face_names.append(name)
 
-    process_this_frame = not process_this_frame
+    if face_names == []:
+        os.system('clear')
+        print("TOKO SEPI GAN")
+    elif "Unknown" in face_names:
+        os.system('clear')
+        print("Anda Siapa ?")
+    else:
+        list_produk = []
+        list_produk = decode_qr(FrameThread.frame_qr)
+        shopping_time = 15
+        os.system('clear')
+        start_time = time.time()
+        print("Halo", face_names[0], "Selamat Berbelanja")
+        print("Saldo Anda", saldo[first_match_index])
+        print("-!!- PROMO HARGA FLAT 1000 SADJA -!!-")
+        print("Produk Yang Tersedia:")
+        for obj in list_produk:
+            print(obj)
+        time.sleep(shopping_time)
+        list_produk_akhir = []
+        list_produk_akhir = decode_qr(FrameThread.frame_qr)
+        jumlah_terambil = len(list_produk)-len(list_produk_akhir)
+        print("Anda mengambil", jumlah_terambil, "barang")
+        print("Total Belanja", jumlah_terambil*1000)
+        saldo[first_match_index] = saldo[first_match_index] - jumlah_terambil*1000
+        print("Saldo Anda Sekarang", saldo[first_match_index])
+        print("Terima Kasih Sudah Berbelanja", face_names[0], "!")
+        time.sleep(5)
 
-
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
-
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-    im = frameqr
- 
-    decodedObjects = decode(im)
-    display(im, decodedObjects)
-
-    # Display the resulting image
-    cv2.imshow('Video', frame)
-
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
+    time.sleep(0.5)
